4aca3471c50527a1fbd05eb60b41f12413f195ee
Vivek <saigoguladinne@gmail.com>
2025-08-14 14:46:01 +0530

    Initiated database SQLite and authentication using werkzeug

---
+++ expense-tracker/models.py
@@ -0,0 +1,38 @@
+import enum
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.ext.hybrid import hybrid_property
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), unique = True, nullable = False)
+    password_hash = db.Column(db.String(256), nullabe = False)
+    expenses = db.relationship("Expense", backref="owner", lazy = True)
+
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+class ExpenseCategory(enum.Enum):
+    FOOD = "Food"
+    TRANSPORT = "Transport"
+    UTILITIES = "Utilities"
+    ENTERTAINMENT = "Entertainment"
+    OTHER = "Other"
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    category = db.Column(db.Enum(ExpenseCategory), nullable=False, default=ExpenseCategory.OTHER)
+    is_reimbursable = db.Column(db.Boolean, default=False, nullable=False)
+    base_amount = db.Column(db.Float, nullable=False)
+    tax_amount = db.Column(db.Float, nullable=False, default=0.0)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    @hybrid_property
+    def total_amount(self):
+        return self.base_amount + self.tax_amount
